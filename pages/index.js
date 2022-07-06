a70c486a485a5f4ca5c0af78aab8ce670b585efa
Ivan Lopez <ivaan100@gmail.com>
2022-07-06 12:41:38 +0200

    Testing commit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import styled from '@emotion/styled'
 
-const Heading = styled.h1`
-    color: red;
-    `;
 const Home = () => (
     <div>
         <Heading>Inicio</Heading>
