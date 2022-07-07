d65405a1710d54c9ca8edb6aeaf30e41ab6f614a
Ivan Lopez <ivaan100@gmail.com>
2022-07-07 12:17:39 +0200

    Adding GoogleFonts

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './Header';
 import { Global, css } from '@emotion/react'
+import Head from 'next/head'
 
 const Layout = props => {
+
     return (
         <>
-            <Global 
+            <Global
                 styles={css`
                     :root {
                         --gris: #3D3D3D;
                         --gris2: #6F6F6F;
+                        --gris3: #e1e1e1;
                         --naranja: #DA552F
                     }
 
@@ -17,7 +20,7 @@ const Layout = props => {
                         font-size: 62.5%;
                         box-sizing: border-box;
                     }
-                    *, *:before, *after {
+                    *, *:before, *:after {
                         box-sizing: inherit;
                     }
                     body {
@@ -27,6 +30,15 @@ const Layout = props => {
                     h1, h2, h3 {
                         margin: 0 0 2rem 0;
                         line-height: 1.5;
+                    }
+
+                    h1, h2 {
+                        font-family: 'Roboto Slab', serif;
+                        font-weight: 700;
+                    }
+
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
                     }
 
                     ul {
@@ -40,14 +52,25 @@ const Layout = props => {
                     }
                 `}
             />
-           <Header />
+
+            <Head>
+                <html lang="es" />
+                <title>Product Hunt Firebase y Next.js</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap" rel="stylesheet" />
+
+            </Head>
+
+            <Header />
 
             <main>
                 {props.children}
             </main>
-            
+
         </>
     );
 }
 
-export default Layout;
+export default Layout
